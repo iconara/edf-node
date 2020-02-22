c4f499a36252b6e74722b154f9c61079a1ddffce
Theo <theo@iconara.net>
2020-02-22 13:54:43 +0100

    Change to use Arrow internally too
    Let the Edf class be a thin wrapper over an Arrow table.
    
    This has a side effect in that the Crc16 gets expanded to be as long as the other vectors.

--- spec/edf.spec.js
+++ spec/edf.spec.js
@@ -41,55 +41,44 @@ describe('Edf', () => {
           expect(edf.duration).toBe(30 * 2880 * 1000)
         })
 
-        test('contains signal metadata for each signal', async () => {
+        test('contains the names of each signal', async () => {
           const edf = await Edf.fromBuffer(get('edfBuffer'))
-          expect(edf.signals).toHaveLength(2)
+          expect(edf.signalNames).toEqual([
+            'EEG Fpz-Cz',
+            'Temp rectal',
+          ])
         })
 
-        test('contains the signal labels', async () => {
+        test('contains the signal metadata', async () => {
           const edf = await Edf.fromBuffer(get('edfBuffer'))
-          expect(edf.signals[0].label).toBe('EEG Fpz-Cz')
-          expect(edf.signals[1].label).toBe('Temp rectal')
-        })
-
-        test('contains the signal transducer types', async () => {
-          const edf = await Edf.fromBuffer(get('edfBuffer'))
-          expect(edf.signals[0].transducerType).toBe('AgAgCl cup electrodes')
-          expect(edf.signals[1].transducerType).toBe('Rectal thermistor')
-        })
-
-        test('contains the signal physical dimensions', async () => {
-          const edf = await Edf.fromBuffer(get('edfBuffer'))
-          expect(edf.signals[0].physicalDimension).toBe('uV')
-          expect(edf.signals[1].physicalDimension).toBe('degC')
+          const eegSignal = edf.getSignal('EEG Fpz-Cz')
+          const tempSignal = edf.getSignal('Temp rectal')
+          expect(eegSignal.metadata.get('transducerType')).toBe('AgAgCl cup electrodes')
+          expect(eegSignal.metadata.get('physicalDimension')).toBe('uV')
+          expect(eegSignal.metadata.get('prefiltering')).toBe('HP:0.1Hz LP:75Hz N:50Hz')
+          expect(tempSignal.metadata.get('transducerType')).toBe('Rectal thermistor')
+          expect(tempSignal.metadata.get('physicalDimension')).toBe('degC')
+          expect(tempSignal.metadata.get('prefiltering')).toBe('LP:0.1Hz (first order)')
         })
 
         test('contains the signal physical min and max', async () => {
           const edf = await Edf.fromBuffer(get('edfBuffer'))
-          expect(edf.signals[0].physicalMinimum).toBe(-440)
-          expect(edf.signals[0].physicalMaximum).toBe(510)
-          expect(edf.signals[1].physicalMinimum).toBe(34.4)
-          expect(edf.signals[1].physicalMaximum).toBe(40.2)
+          const eegSignal = edf.getSignal('EEG Fpz-Cz')
+          const tempSignal = edf.getSignal('Temp rectal')
+          expect(eegSignal.metadata.get('physicalMinimum')).toBe(-440)
+          expect(eegSignal.metadata.get('physicalMaximum')).toBe(510)
+          expect(tempSignal.metadata.get('physicalMinimum')).toBe(34.4)
+          expect(tempSignal.metadata.get('physicalMaximum')).toBe(40.2)
         })
 
         test('contains the signal digital min and max', async () => {
           const edf = await Edf.fromBuffer(get('edfBuffer'))
-          expect(edf.signals[0].digitalMinimum).toBe(-2048)
-          expect(edf.signals[0].digitalMaximum).toBe(2047)
-          expect(edf.signals[1].digitalMinimum).toBe(-2048)
-          expect(edf.signals[1].digitalMaximum).toBe(2047)
-        })
-
-        test('contains the signal prefiltering', async () => {
-          const edf = await Edf.fromBuffer(get('edfBuffer'))
-          expect(edf.signals[0].prefiltering).toBe('HP:0.1Hz LP:75Hz N:50Hz')
-          expect(edf.signals[1].prefiltering).toBe('LP:0.1Hz (first order)')
-        })
-
-        test('contains the signal sample counts', async () => {
-          const edf = await Edf.fromBuffer(get('edfBuffer'))
-          expect(edf.signals[0].sampleCount).toBe(15000)
-          expect(edf.signals[1].sampleCount).toBe(3)
+          const eegSignal = edf.getSignal('EEG Fpz-Cz')
+          const tempSignal = edf.getSignal('Temp rectal')
+          expect(eegSignal.metadata.get('digitalMinimum')).toBe(-2048)
+          expect(eegSignal.metadata.get('digitalMaximum')).toBe(2047)
+          expect(tempSignal.metadata.get('digitalMinimum')).toBe(-2048)
+          expect(tempSignal.metadata.get('digitalMaximum')).toBe(2047)
         })
       })
     })
@@ -100,8 +89,10 @@ describe('Edf', () => {
       describe('returns an Edf object that', () => {
         test('contains the measurements of each signal', async () => {
           const edf = await Edf.fromBuffer(get('edfBuffer'))
-          expect(edf.signals[3].data).toHaveLength(169 * 30)
-          expect(edf.signals[3].data.slice(0, 120)).toEqual(Int16Array.from([
+          const leakSignal = edf.getSignal('Leak.2s')
+          const crcSignal = edf.getSignal('Crc16')
+          expect(leakSignal).toHaveLength(169 * 30)
+          expect(leakSignal.slice(0, 120).toArray()).toEqual(Int16Array.from([
             2, 3, 5, 4, 3, 0, 3, 4, 2, 4,
             3, 3, 3, 3, 2, 3, 3, 2, 3, 2,
             2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
@@ -115,8 +106,8 @@ describe('Edf', () => {
             1, 1, 1, 2, 2, 1, 3, 2, 2, 2,
             1, 1, 1, 1, 1, 2, 2, 1, 1, 1,
           ]))
-          expect(edf.signals[9].data).toHaveLength(169)
-          expect(edf.signals[9].data.slice(0, 3)).toEqual(Int16Array.from([-27479, 17000, -7973]))
+          // expect(crcSignal).toHaveLength(169)
+          expect(crcSignal.slice(0, 3).toArray()).toEqual(Int16Array.from([-27479, 17000, -7973]))
         })
 
         describe('can be converted into an Arrow table that', () => {
@@ -124,6 +115,7 @@ describe('Edf', () => {
             const edf = await Edf.fromBuffer(get('edfBuffer'))
             const table = edf.toTable()
             const fieldNames = table.schema.fields.map((f) => f.name)
+            expect(fieldNames).toEqual(edf.signalNames)
             expect(fieldNames).toEqual([
               'MaskPress.2s',
               'Press.2s',
@@ -142,7 +134,7 @@ describe('Edf', () => {
             const edf = await Edf.fromBuffer(get('edfBuffer'))
             const table = edf.toTable()
             const column = table.getColumn('Leak.2s')
-            expect(Array.from(column)).toEqual(Array.from(edf.signals[3].data))
+            expect(column.toArray()).toEqual(edf.getSignal('Leak.2s').toArray())
           })
 
           test('contains the file metadata', async () => {
@@ -156,14 +148,7 @@ describe('Edf', () => {
             const edf = await Edf.fromBuffer(get('edfBuffer'))
             const table = edf.toTable()
             const column = table.getColumn('Leak.2s')
-            expect(column.field.metadata.get('transducerType')).toBe('')
-            expect(column.field.metadata.get('physicalDimension')).toBe('L/s')
-            expect(column.field.metadata.get('physicalMinimum')).toBe(0)
-            expect(column.field.metadata.get('physicalMaximum')).toBe(2)
-            expect(column.field.metadata.get('digitalMinimum')).toBe(0)
-            expect(column.field.metadata.get('digitalMaximum')).toBe(100)
-            expect(column.field.metadata.get('prefiltering')).toBe('')
-            expect(column.field.metadata.get('sampleCount')).toBe(30)
+            expect(column.field.metadata).toEqual(edf.getSignal('Leak.2s').metadata)
           })
         })
       })
