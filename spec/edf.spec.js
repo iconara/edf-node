456d8d7721ee4dcbb4fb3fb69d5e27e74f95980f
Theo <theo@iconara.net>
2020-02-22 11:41:41 +0100

    Switch to a EDF file with more signals and measurements

--- spec/edf.spec.js
+++ spec/edf.spec.js
@@ -90,14 +90,21 @@ describe('Edf', () => {
     })
 
     describe('with EDF+ data that contains signal measurements', () => {
-      def('edfBuffer', () => fs.readFileSync(__dirname + '/resources/20190828_032858_SAD.edf'))
+      def('edfBuffer', () => fs.readFileSync(__dirname + '/resources/20200115_231509_PLD.edf'))
 
       describe('returns an Edf object that', () => {
         test('contains the measurements of each signal', async () => {
           const edf = await Edf.fromBuffer(get('edfBuffer'))
-          expect(edf.signals[0].data).toStrictEqual(Int16Array.from([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]))
-          expect(edf.signals[1].data).toStrictEqual(Int16Array.from([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]))
-          expect(edf.signals[2].data).toStrictEqual(Int16Array.from([-9418]))
+          expect(edf.signals[0].data).toStrictEqual(Int16Array.from([181, 201, 204, 206, 205, 203, 204, 204, 201, 203, 203, 202, 201, 201, 199, 201, 200, 200, 201, 201, 200, 201, 200, 199, 200, 200, 199, 199, 200, 199]))
+          expect(edf.signals[1].data).toStrictEqual(Int16Array.from([200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200]))
+          expect(edf.signals[2].data).toStrictEqual(Int16Array.from([200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200]))
+          expect(edf.signals[3].data).toStrictEqual(Int16Array.from([  2,   3,   5,   4,   3,   0,   3,   4,   2,   4,   3,   3,   3,   3,   2,   3,   3,   2,   3,   2,   2,   2,   2,   2,   2,   2,   2,   2,   2,   2]))
+          expect(edf.signals[4].data).toStrictEqual(Int16Array.from([  0,   0,   0,   0,   0,   0, 101, 101, 101,  91,  91,  91,  68,  68,  68,  62,  62,  62,  60,  60,  60,  48,  48,  48,  48,  48,  48,  47,  46,  46]))
+          expect(edf.signals[5].data).toStrictEqual(Int16Array.from([  0,   0,   0,   0,   0,   0,  33,  33,  33,  35,  35,  35,  45,  45,  45,  41,  41,  41,  43,  43,  43,  51,  51,  51,  47,  47,  47,  49,  46,  46]))
+          expect(edf.signals[6].data).toStrictEqual(Int16Array.from([  0,   0,   0,   0,   0,   0, 108, 108, 108, 102, 102, 102,  98,  98,  98,  82,  82,  82,  84,  84,  84,  79,  79,  79,  72,  72,  72,  74,  69,  69]))
+          expect(edf.signals[7].data).toStrictEqual(Int16Array.from([  0,   0,   0,   0,   0,   0,   1,   1,   1,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0]))
+          expect(edf.signals[8].data).toStrictEqual(Int16Array.from([  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0]))
+          expect(edf.signals[9].data).toStrictEqual(Int16Array.from([-27479]))
         })
       })
     })
